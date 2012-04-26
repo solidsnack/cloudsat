143bbb917481d774298c8d09bfc74201c878b77f
Jason Dusek <jason.dusek@gmail.com>
2012-04-26 01:43:19 +0000

    Work in progress. Address normalization, subscriptions, registration and connections to the Postgres activity list.

--- cloudsat.sql
+++ cloudsat.sql
@@ -1,3 +1,4 @@
+DROP SCHEMA cloudsat CASCADE;
 CREATE EXTENSION "uuid-ossp"; -- Needed for stored procedure. Loaded here so
                               -- we fail before making any changes to the
                               -- database if it's not available.
@@ -16,9 +17,6 @@ CREATE INDEX ON messages USING hash(poster);
 CREATE INDEX ON messages USING hash(chan);
 CREATE INDEX ON messages USING gist(to_tsvector('english', message));
 
-CREATE TYPE disposition AS ENUM
-( 'ignored', 'acknowledged', 'info', 'problem', 'done' );
-
 CREATE TABLE threads
 ( before      uuid NOT NULL REFERENCES messages(uuid),
   disposition disposition NOT NULL,
@@ -29,14 +27,30 @@ CREATE INDEX ON threads USING hash(disposition);
 CREATE INDEX ON threads (after);
 COMMENT ON TABLE threads IS 'Links messages with their replies.';
 
+CREATE TABLE registered
+( procpid       integer NOT NULL,
+  backend_start timestamp with time zone NOT NULL,
+  timestamp     timestamp with time zone NOT NULL,
+  chans         text[] NOT NULL );
+CREATE INDEX ON registered (procpid);
+CREATE INDEX ON registered (backend_start);
+CREATE INDEX ON registered (timestamp);
+CREATE INDEX ON registered (chans);
+COMMENT ON TABLE registered IS
+ 'Explicit information about connected clients and their subscriptions.';
+
+CREATE TYPE disposition AS ENUM
+( 'ignored', 'acknowledged', 'info', 'problem', 'done' );
+
 
 CREATE FUNCTION post(poster text, chan text, message text)
 RETURNS uuid AS $$
 DECLARE
-  id uuid;
+  id    uuid := uuid_generate_v1();
+  chan_ text := norm(chan);
 BEGIN
-  SELECT uuid_generate_v1() INTO id;
-  INSERT INTO messages VALUES (id, NOW(), poster, chan, message); 
+  INSERT INTO messages VALUES (id, NOW(), norm(poster), chan_, message); 
+  PERFORM pg_notify(chan_, id::text);
   RETURN id;
 END;
 $$ LANGUAGE plpgsql STRICT;
@@ -49,7 +63,7 @@ RETURNS uuid AS $$
 DECLARE
   id uuid;
 BEGIN
-  SELECT post(poster, chan, message) INTO id;
+  id := post(poster, chan, message);
   INSERT INTO threads VALUES (parent, disposition, id);
   RETURN id;
 END;
@@ -60,21 +74,42 @@ COMMENT ON FUNCTION reply
 
 CREATE FUNCTION posts(chans text[])
 RETURNS SETOF messages AS $$
-BEGIN
-  RETURN QUERY SELECT * FROM messages WHERE chan = ANY (chans);
-END;
-$$ LANGUAGE plpgsql STRICT;
+SELECT * FROM messages WHERE chan = ANY ($1);
+$$ LANGUAGE sql STRICT;
 COMMENT ON FUNCTION posts(chans text[]) IS
  'Searches for posts in the given channels.';
+
+CREATE FUNCTION register(addresses text[])
+RETURNS VOID AS $$
+BEGIN
+  PERFORM subscribe(suffixes(addresses));
+END;
+$$ LANGUAGE plpgsql STRICT;
+COMMENT ON FUNCTION register(addresses text[]) IS
+ 'Create subscriptions and register client.';
+
+CREATE FUNCTION subscribe(chans text[])
+RETURNS VOID AS $$
+DECLARE
+  chan text;
+BEGIN
+  FOREACH chan IN ARRAY chans LOOP
+    EXECUTE 'LISTEN ' || quote_ident(chan);
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql STRICT;
+COMMENT ON FUNCTION subscribe(chans text[]) IS
+ 'Creates subscriptions for a list of channels.';
+
 
 CREATE FUNCTION norm(address text)
 RETURNS text AS $$
 BEGIN
-  RETURN lower(trim(trailing '.' from address)) || '.';
+  RETURN trim(leading '.' from lower(trim(trailing '.' from address))) || '.';
 END;
 $$ LANGUAGE plpgsql STRICT;
 COMMENT ON FUNCTION norm(address text) IS
- 'Normalize an address so it has the final dot.';
+ 'Normalize an address so it is lowercase and has the final dot.';
 
 CREATE FUNCTION suffixes(address text)
 RETURNS text[] AS $$
@@ -105,4 +140,29 @@ $$ LANGUAGE plpgsql STRICT;
 COMMENT ON FUNCTION suffixes(address text) IS
  'Break an address like admin@example.com into rooted pieces like this:
   {com., example.com., admin@example.com.}';
+
+CREATE FUNCTION suffixes(addresses text[])
+RETURNS text[] AS $$
+DECLARE
+  acc     text[];
+  address text;
+BEGIN
+  FOREACH address IN ARRAY addresses LOOP
+    acc := acc || suffixes(address);
+  END LOOP;
+  RETURN sort_uniq(acc);
+END;
+$$ LANGUAGE plpgsql STRICT;
+COMMENT ON FUNCTION suffixes(addresses text[]) IS
+ 'Returns the unions of all suffixes of all the addresses.';
+
+CREATE FUNCTION sort_uniq(ANYARRAY)
+RETURNS ANYARRAY AS $$
+SELECT ARRAY( SELECT DISTINCT $1[s.i] AS "foo"
+              FROM
+                generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+              ORDER BY foo );
+$$ LANGUAGE sql IMMUTABLE;
+COMMENT ON FUNCTION sort_uniq(ANYARRAY) IS
+ 'The equivalent of sort | uniq, it ensures an array contains no duplicates.';
 
